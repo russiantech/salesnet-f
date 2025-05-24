2785ae8020678b50a8f4467a81c176acb7ad1b35
russiantech <chrisjsmez@gmail.com>
2025-05-24 19:19:47 +0100

    downngraded to react v18

--- src/pages/vendor/dashboard/Dashboard.tsx
+++ src/pages/vendor/dashboard/Dashboard.tsx
@@ -1,5 +1,4 @@
-import { Helmet } from 'react-helmet'; // Import Helmet
-// import Aside from '../../user/shared/Aside'
+import { Helmet } from 'react-helmet-async';
 import Navigation from '../../../components/shared/Navigation'
 import Aside from '../shared/Aside'
 
